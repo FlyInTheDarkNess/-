ee24e9e1c3529af0db4f87a8dc0344e5d2cb279b
赵中良 <mizilang@mizilangtekiMacBook-Air.local>
2014-09-03 11:33:50 +0800

    Initial Commit

---
+++ 日租帮/IuiueAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IuiueAppDelegate.h
+//  日租帮
+//
+//  Created by 赵中良 on 14-9-3.
+//  Copyright (c) 2014年 com.iuiue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IuiueAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 日租帮/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  日租帮
+//
+//  Created by 赵中良 on 14-9-3.
+//  Copyright (c) 2014年 com.iuiue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IuiueAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IuiueAppDelegate class]));
+    }
+}
